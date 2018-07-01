cc70de2af099b70ca395c6a248b4f9fd6045bf72
Ryomou <sandrawanjofu@gmail.com>
2018-07-01 21:46:08 +0300

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,23 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+      var height = document.getElementById('input_height').value;
+      var width = document.getElementById('input_width').value;
+      var table = document.getElementById('pixel_canvas');
+  	// Table grid creation code :
+      table.innerHTML = '';
+      var tbody = document.createElement('tbody');
+      for (var i = 0; i < height; i++) {
+          var tr = document.createElement('tr');
+          for (var j = 0; j < width; j++) {
+              var td = document.createElement('td');
+              td.appendChild(document.createTextNode(''));
+              tr.appendChild(td);
+          }
+          tbody.appendChild(tr);
+      }
+      table.appendChild(tbody);
+};
